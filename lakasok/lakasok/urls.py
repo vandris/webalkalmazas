dd28ee54aca6e3cf42db3ee31cf8310c6f674d88
papnori <nori.753@gmail.com>
2017-10-13 17:48:39 +0200

    kok

--- lakasok/lakasok/urls.py
+++ lakasok/lakasok/urls.py
@@ -1,11 +1,11 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib import admin
 
-urlpatterns = patterns('',
+urlpatterns = [
     # Examples:
     # url(r'^$', 'lakasok.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-	#url(r'^polls/', include('polls.urls')),
-    url(r'^admin/', include(admin.site.urls)),
-)
+	url(r'^polls/', include('polls.urls')),
+    url(r'^admin/', admin.site.urls),
+]
