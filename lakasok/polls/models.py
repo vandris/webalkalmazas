ad7ab6d152345d05ff143caa62e8578bcadf9db6
papnori <nori.753@gmail.com>
2017-10-13 18:38:11 +0200

    kok

--- lakasok/polls/models.py
+++ lakasok/polls/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+class Users(models.Model):
+    email = models.CharField(max_length=200, unique=True)
+    fullname = models.CharField(max_length=200)
+    password = models.CharField(max_length=50)
+    gender = models.CharField(max_length=10)
+    age = models.IntegerField(default=40)
+    valid = models.BooleanField(default=True)
+
+class Adds(models.Model):
+    owner = models.CharField(max_length=200)
+    squaremeter = models.IntegerField(default=40)
+    price = models.IntegerField(default=10000)
+    type = models.CharField(max_length=50)
+    wall = models.CharField(max_length=50)
+    heating = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    rooms = models.IntegerField(default=3)
+    parking = models.CharField(max_length=100)
+    year = models.IntegerField(default=2017)
+    furnitured = models.BooleanField(default=False)
+    lift = models.BooleanField(default=False)
+    view = models.CharField(max_length=50)
+
+
+class Messages(models.Model):
+    sender = models.CharField(max_length=200)
+    receiver = models.CharField(max_length=200)
+    message = models.CharField(max_length=5000)
+    valid = models.BooleanField(default=True)
+    unread = models.BooleanField(default=True)
